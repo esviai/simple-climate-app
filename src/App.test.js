d7567100426967a33b83b3921f8cec207d5eda3f
ingelieur <ingelieur@gmail.com>
2017-07-28 01:25:13 +0700

    main layout TDD

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+import App from './App'
+
+describe('<App> component ', () => {
+
+/*
+==================================================================
+OUTPUT TEST
+Shallow testing main parts that should exist in the app
+==================================================================
+*/
+
+  const appWrapper = shallow(<App />)
+
+  it('renders correctly', () => {
+    expect(appWrapper).toHaveLength(1)
+  })
+
+  it('renders a <section>', () => {
+    const appSection = appWrapper.find('section')
+    expect(appSection).toHaveLength(1)
+  })
+
+  it('renders a form', () => {
+    const appForm = appWrapper.find('.field')
+    expect(appForm).toHaveLength(1)
+  })
+
+  it('renders a table', () => {
+    const appTable = appWrapper.find('table')
+    expect(appTable).toHaveLength(1)
+  })
+})
+
